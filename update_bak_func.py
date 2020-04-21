70ee69f39c61b48a7609c5f78a4ced8725e31c9d
Exluso <pg.miceli@gmail.com>
2020-04-21 20:01:17 +0200

    update_bak_func removes folders and files

--- update_bak_func.py
+++ update_bak_func.py
@@ -1,32 +1,33 @@
 import shutil
 import os
+import send2trash
+import lithar_backup as lb
 from wording import curtain  # remove at the end
 
 
 def update_bak(source, dest):
-    """Finally this function updates the file in the backup folder."""
+    """Finally this function updates the file in the backup folder.
+    Works in 3 phases:
+    1 - detects changes
+    2 - takes action to match the changes in the backup
+    3 - reports the changes
+    for each phase it handles in this order: adds new items, updates old items,
+    removes obsolete items."""
     # todo a function that replaces outdated files with more recent ones.
-    # todo a function that removes files and folder from back if the same
-    #  files and folders are not in the source anymore
-    # todo a function that adds files in the backup if they are in the
-    #  source and not in the backup
+
     print("source:", source)
     print("dest:", dest)
+
     files_added = {}
     files_removed = {}
     dir_added = []
-    dir_removed = {}
+    dir_removed = []
     for active_dir, subfolders, filenames in os.walk(source):
-        #print("active dir:", active_dir)
-        #print("\tsubfodlers:", subfolders)
-        #print("\tfiles:", filenames)
-
         # mirror_dir mirrors the walking in the source tree into the dest tree
         mirror_dir = os.path.join(dest,
                                   "" if active_dir == source
                                   else os.path.relpath(active_dir, source))
 
-        #print("mirror list:", os.listdir(mirror_dir))
         mirror_filenames = [mf for mf in os.listdir(mirror_dir)
                             if os.path.isfile(os.path.join(mirror_dir, mf))]
         mirror_subfolders = [msf for msf in os.listdir(mirror_dir)
@@ -35,50 +36,66 @@ def update_bak(source, dest):
         # Detects new stuff
         new_files = set(filenames) - set(mirror_filenames)
         new_subfolders = set(subfolders) - set(mirror_subfolders)
-        curtain()
-        print("original", subfolders)
-        print("mirror", mirror_subfolders)
-        print("new:", new_subfolders)
+        # Detects obsolete stuff (the ones that have to be removed)
+        obsolete_files = set(mirror_filenames) - set(filenames)
+        obsolete_subfolders = set(mirror_subfolders) - set(subfolders)
+
         curtain()
 
-        # updates new stuff
+        # Adds new stuff
+        for nsf in new_subfolders:
+            new_dir = os.path.join(mirror_dir, nsf)
+            shutil.copytree(os.path.join(active_dir, nsf), new_dir)
+            dir_added.append(nsf)
+
         local_new_files = []
         for nf in new_files:
-            print("source:", source)
-            print("fileO", os.path.join(active_dir, nf))
-            print("dirDest", dest)
-            print("mirror:", mirror_dir)
             shutil.copy2(os.path.join(active_dir, nf), mirror_dir)
             local_new_files.append(nf)
             files_added.update({mirror_dir: local_new_files})
 
-        local_new_sub_folders = []
-        for nsf in new_subfolders:
-            # todo a function that copies a folder and all its contents
-            # todo idea: mkdir and then shutil.copytree?
-            print("orig fold:", os.path.join(active_dir, nsf))
-            print("dest fold:", mirror_dir)
-            new_dir = os.path.join(mirror_dir, nsf)
-            #os.makedirs(new_dir)
-            shutil.copytree(os.path.join(active_dir, nsf), new_dir)
-            dir_added.append(nsf)
+        # Removes obsolete stuff
+        for osf in obsolete_subfolders:
+            to_the_bin = shutil.move(os.path.join(mirror_dir, osf),
+                                     os.path.join(mirror_dir, osf + "_bak"))
+            send2trash.send2trash(to_the_bin)
+            dir_removed.append(to_the_bin[:-4])  # removes "_bak" from the print
 
-
+        local_removed_files = []
+        for outdated_file in obsolete_files:
+            to_the_bin = shutil.move(os.path.join(mirror_dir, outdated_file),
+                                     os.path.join(mirror_dir, outdated_file
+                                                  + "_bak"))
+            send2trash.send2trash(to_the_bin)
+            local_removed_files.append(outdated_file)
+            files_removed.update({mirror_dir: local_removed_files})
 
         #debugs
-        print("filenames:", filenames)
-        print("mirror files:", mirror_filenames)
-        print("new files:", new_files)
+        # print("filenames:", filenames)
+        # print("mirror files:", mirror_filenames)
+        # print("new files:", new_files)
 
     # Reports changes
-    print("The following subfolders have been added with their content:")
-    for f in dir_added:
-        print("\t", f)
+    if dir_added:
+        print("The following subfolders have been added with their content:")
+        for f in dir_added:
+            print("\t", f)
 
     for k in files_added:
         print("new addition in " + k + ":")
         for file in files_added[k]:
             print("\t" + file)
+
+    if dir_removed:
+        print("The following directories, including their contents,"
+              " have been removed:")
+        for folder in dir_removed:
+            print("\t", folder)
+
+    for k in files_removed:
+        print("File removed from " + k + ":")
+        for file in files_removed[k]:
+            print("\t", file)
 
 
 if __name__ == "__main__":
@@ -86,5 +103,7 @@ if __name__ == "__main__":
     t_dest = os.path.join("C:\\", "Users", "pgmic", "Desktop", "CT_test")
     update_bak(t_source, t_dest)
 
-# TODO clean from debug detecting and adding new file and folders
+# TODO handling the updates in each phase
+# todo TESTING TESTING MAKE TEST CASES!
 # TODO replace the hardcoded text in Report changes
+# TODO rewrite the code to fit in lithar, using the lithar_backup (bak) objects
